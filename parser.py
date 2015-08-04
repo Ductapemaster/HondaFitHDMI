d3f77f5f0d872c4cea17d8ae81faf3988946daed
Ductapemaster <Ductapemaster@gmail.com>
2015-08-04 08:17:45 -0700

    Playing around with the parser.  Prints out some debug info now, and also a list of unique addresses and how many frames occured with that specific address

--- parser.py
+++ parser.py
@@ -1,26 +1,51 @@
 #!/usr/bin/python
 
 import sys
+from sets import Set
 
 f = open(sys.argv[1])
 
 class canframe:
 
     def __init__(self, time, addr, payload):
-        self.time=time
-        self.addr=addr
-        self.payload=payload
+        self.timeStr=time
+        self.addrStr=addr
+        self.payloadStr=payload
+        self.addrBytes =  bytearray.fromhex(self.addrStr)
+        self.payloadBytes = bytearray.fromhex(self.payloadStr)
+
+    def __str__(self):
+        return "Time: " + self.timeStr + "\nAddr: " + self.addrStr +"\nData: " + self.payloadStr
+        
 
 frames=[]
+addrSet = Set()
+fDict = {}
 
 for line in f:
     s = line.split()
     t = s[0].strip('()')
     a = s[2].split('#')[0]
     p = s[2].split('#')[1]
+
+    if a not in addrSet:
+        fDict[a] = []
+
+    addrSet.add(a)
+    fDict[a].append( canframe(t, a, p) )
     frames.append( canframe(t, a, p) )
 
-print "Time: ", frames[0].time
-print "Addr: ", frames[0].addr
-print "Data: ", frames[0].payload
 
+print frames[0]
+print ""
+print "Hex Addr: ", int("0x" + frames[0].addrStr, 16)
+print "Hex Data: ", int("0x" + frames[0].payloadStr, 16)
+print ""
+print "Hex Addr: ", len(bytearray.fromhex(frames[0].addrStr))
+print "Hex Data: ", len(bytearray.fromhex(frames[0].payloadStr))
+
+for key in fDict:
+    print fDict[key][0].addrStr, ": ", len(fDict[key])
+
+#for item in fDict['20000004']:
+#    print item.timeStr, ": ", item.payloadStr
