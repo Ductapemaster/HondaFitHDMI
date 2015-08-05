a683b85bac8a4f4568faae96003f7058796e32c4
Ductapemaster <Ductapemaster@gmail.com>
2015-08-04 20:14:18 -0700

    Refactored the entropy function to be more generic.  Renamed some variables.

--- parser.py
+++ parser.py
@@ -4,21 +4,21 @@ import sys
 from sets import Set
 
 # Could make an object for a collection of canframes:
-# - One address, multiple frames - could be called "canDevice"?
+# - One arbitration ID, multiple frames - could be called "canDevice"?
 # - Could merge entropy calculation into it
 
 class canframe:
 
-    def __init__(self, time, addr, payload):
+    def __init__(self, time, arbId, payload):
         
         # String data
         self.timeStr=time
-        self.addrStr=addr
+        self.arbIdStr=arbId
         self.payloadStr=payload
-        self.csv = time + ',' + addr + ',' + payload + '\n'
+        self.csv = time + ',' + arbId + ',' + payload + '\n'
         
         # Parse data into bytes
-        self.addrBytes =  bytearray.fromhex(self.addrStr)
+        self.arbIdBytes =  bytearray.fromhex(self.arbIdStr)
         self.payloadBytes = bytearray.fromhex(self.payloadStr)
         self.payloadByteStr = []
 
@@ -28,38 +28,41 @@ class canframe:
         self.payloadByteStr = ','.join(self.payloadByteStr)
 
     def __str__(self):
-        return "Time: " + self.timeStr + "\nAddr: " + self.addrStr +"\nData: " + self.payloadStr
+        return "Time: " + self.timeStr + "\narbId: " + self.arbIdStr +"\nData: " + self.payloadStr
 
     def makeCsv(self):
-        #return ','.join([self.timeStr, self.addrStr, self.payloadStr]) + '\n'
-        return ','.join([self.timeStr, self.addrStr, self.payloadStr, self.payloadByteStr]) + '\n'
+        #return ','.join([self.timeStr, self.arbIdStr, self.payloadStr]) + '\n'
+        return ','.join([self.timeStr, self.arbIdStr, self.payloadStr, self.payloadByteStr]) + '\n'
         
 def writeCSV(frameArray, filename):
     fo = open(filename, 'w+')
     for item in frameArray:
         fo.write( item.makeCsv() )
 
-def findEntropy(dictionary, interestingOnly):
-    for addr in dictionary:
-        e = Set()
-        for frame in dictionary[addr]:
-            e.add( frame.payloadStr )
-        entropy = float(len(e)) / float(len(dictionary[addr]))
-        if interestingOnly:
-            if len(e) > 1 and len(dictionary[addr]) > 1:
-                print '0x{0} Unique: {2:4d}, Total: {3:4d} Entropy: {1:.2%}'.format(addr, entropy, len(e), len(dictionary[addr]))
-        else:
-                print '0x{0} Unique: {2:4d}, Total: {3:4d} Entropy: {1:.2%}'.format(addr, entropy, len(e), len(dictionary[addr]))
+def findPayloadEntropy(canFrameArray):
 
-def uniqueAddresses(dictionary):
+    e = Set()
+
+    for frame in canFrameArray:
+        e.add( frame.payloadStr )
+
+    entropy = float( len( e ) ) / float( len( canFrameArray ) )
+    
+    return e, entropy
+
+
+def uniqueArbIDs(dictionary):
+
     l = []
+
     for key in dictionary:
         l.append(key)
+
     return l
 
 def parseLog(f):
 
-    addrSet = Set()
+    arbIdSet = Set()
 
     for line in f:
         s = line.split()
@@ -67,10 +70,10 @@ def parseLog(f):
         a = s[2].split('#')[0]
         p = s[2].split('#')[1]
 
-        if a not in addrSet:
+        if a not in arbIdSet:
             frameDict[a] = []
 
-        addrSet.add(a)
+        arbIdSet.add(a)
         frameDict[a].append( canframe(t, a, p) )
         frames.append( canframe(t, a, p) )
 
@@ -78,19 +81,22 @@ def parseLog(f):
 ### Parse argument for log file and directory
 path = sys.argv[1].rpartition('/')
 workingDir = path[0] + '/'
-filename = path[2]
+inFile = path[2]
 
 ### Variables
-frames=[]
-frameDict = {}
-interestingOnly = True  # makes entropy command only print out addresses with data entropy > 1
+frames=[]               # Generic parsed form of data
+frameDict = {}          # Dictionary of CAN Frames accessed by arbitration ID:
+interestingOnly = True  # makes entropy command only print out Arbitration IDs with data entropy > 1
 
-parseLog( open( workingDir + filename ) )
+parseLog( open( workingDir + inFile ) )
 
-# This function should return some sort of list or dictionary, which we then can print.
-findEntropy( frameDict, interestingOnly )
+# Look through dictionary and find interesting values
+for arbId in frameDict:
+    payloadSet, entropy = findPayloadEntropy( frameDict[arbId] )
+    if len(payloadSet) > 1 and len( frameDict[arbId] ) > 1:
+        print '0x{0} Unique: {2:4d}, Total: {3:4d} Entropy: {1:.2%}'.format(arbId, entropy, len(payloadSet), len( frameDict[arbId] ))
 
 # Parse all data into a CSV file.  Keeps same name as log file.
-csvFile = workingDir + '/' + filename[:len(filename)-3] + 'csv'
+csvFile = workingDir + '/' + inFile[:len(inFile)-3] + 'csv'
 writeCSV(frames, csvFile)
 print "Parsed log into CSV: ", csvFile
